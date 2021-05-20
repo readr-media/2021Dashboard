a8eb8259c00d0d7e25d040979aa989aef027892e
andyhuang <andyhuang@mirrormedia.mg>
2021-05-20 12:10:55 +0800

    wip(power): today/yesterday, monthly peak

--- data_format.py
+++ data_format.py
@@ -1,24 +1,24 @@
 import io
 import json
-from datetime import datetime
+from datetime import datetime, timedelta, date
 import requests
 import pandas as pd
 from power_fetcher import power_data_fetcher
+from water_warning import water_warning
 from utils import gsutil_upload
 
-NEWS = "https://storage.googleapis.com/projects.readr.tw/dashboard_covid_news.json"
-covid = "https://raw.github.com/readr-media/readr-data/master/covid-19/indigenous_case_county.csv"
-last_year_peak = 3802.01
 
-file_name = "dashboard.json"
+covid = "https://raw.github.com/readr-media/readr-data/master/covid-19/indigenous_case_county.csv"
+reservoir = "https://storage.googleapis.com/projects.readr.tw/taiwan-dashboard/reservoir.json"
+power_peak = "https://raw.githubusercontent.com/readr-media/readr-data/master/electric-power/monthly_peak.csv"
+last_year_peak = 3802.01
+yesterday = date.today() - timedelta(days=1)
+dashboard_output = "dashboard.json"
 
 def news_fetcher():
-    r = requests.get(NEWS)
+    news = "https://storage.googleapis.com/projects.readr.tw/dashboard_covid_news.json"
+    r = requests.get(news)
     news = r.json()[-1]
-    # news["title"] = news.pop("標題")
-    # news["update_time"] = news.pop("更新時間")
-    # news["author"] = news.pop("記者")
-    # news["content"] = news.pop("內文")
     return news
 
 def covid_data_fetcher():
@@ -29,15 +29,28 @@ def covid_data_fetcher():
         city[k] = int(v)
     return {"today": int(df.iloc[-1][1:-1].sum()), "city": city, "update_time": df.iloc[-1][0]}
 
+
+def power_month_peak():
+    """Get last month power peak"""
+    r = requests.get(power_peak).content
+    df = pd.read_csv(io.StringIO(r.decode('utf-8')))
+    this_month = datetime.now().month
+    power_max = df[df.month==this_month].peak.max() # str
+
+    return float(power_max.replace(',',''))
+
 def power_data_exporter():
+    """Read from GCS and pack the data"""
     
     r = requests.get("https://storage.googleapis.com/projects.readr.tw/power.json")
-    
-    power_by_hr = r.text.split('\n')
+    power_by_hr = r.json()
 
+    power_24h_yesterday = [item for item in power_by_hr if datetime.strptime(item['time'],'%Y-%m-%d %H:%M').date()==yesterday ]
+    power_24h_today = [item for item in power_by_hr if datetime.strptime(item['time'],'%Y-%m-%d %H:%M').date()==date.today() ]
     power_json = {
-        "power_24h":  power_by_hr, # power data within 24hr by hour
-        # "month_peak" : month_peak,
+        "power_24h_yesterday": power_24h_yesterday
+        "power_24h_today":  power_24h_today, # power data within 24hr by hour
+        "month_peak" : power_month_peak(),
         "last_year_peak": last_year_peak,
         "update_time": datetime.now().strftime("%Y-%m-%d") # update time then power_24h has data appended
     }
@@ -45,20 +58,15 @@ def power_data_exporter():
     return power_json
 
 def water_data_fetcher():
-    water = {
-            "N":[],
-            "C":[],
-            "S":[],
-            "E":[],
-        "update_time": datetime.now()
-    }
-    return water
+    r = requests.get(reservoir)
+    r.json().update({"warning":water_warning()})
+    return r.json()
 
 
 def export_data():
 
     data = {"news": news_fetcher(),
-    # "water": water_data_fetcher(),
+    "water": water_data_fetcher(),
     "power": power_data_exporter(),
     "covid": covid_data_fetcher()
     }
@@ -67,10 +75,10 @@ def export_data():
 def main():
     data = export_data()
 
-    with open(file_name, 'w') as f:
+    with open(dashboard_output, 'w') as f:
         f.write(json.dumps(data, ensure_ascii=False).encode('utf8').decode()+'\n')
 
-    gsutil_upload(f"./{file_name}")
+    gsutil_upload(f"./{dashboard_output}")
 
 if __name__ == "__main__":
     # power_data_fetcher()
