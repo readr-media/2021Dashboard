2db9ca81563802429ec8ab257ab907f98497a1a4
andyhuang <andyhuang@mirrormedia.mg>
2021-05-20 11:17:08 +0800

    chg(upload): print location

--- utils.py
+++ utils.py
@@ -2,3 +2,4 @@ import os
 
 def gsutil_upload(output_file_name):
     os.system(f'gsutil -m -h "Content-Type:application/json" -h "Cache-Control:max-age=600,public" cp -z gzip -a public-read -r {output_file_name}  gs://projects.readr.tw/')
+    print(f"https://storage.googleapis.com/projects.readr.tw/{output_file_name}")
